64ec36ea1a9143f49b9d2a768a72457b1bc52b12
MarinaLiakh <liakh.gomel@gmail.com>
2023-03-23 11:18:02 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// здесь jsX пишем НЕ html
+
+
+const TEXT = 'text from const';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  /* У элементов д.б. только один родительский эл-т
+      Eсли надо несколько div, то надо все div обернуть в обёрточный div
+<React.Fragment> (или <>) существует для ситуаций, когда 2 div д.б.рядом , но у них не д.б. обёртки */
+  <React.Fragment>
+    <div id='one' className="oneClass"> 
+        {/* <div id='one' className = { 2 > 1 ? 'o' : 't'}> */}
+        {/* <h1> Hello world! </h1> */}
+        {/* <h1> { [1,2,3].map() } </h1> */}
+        {/* <h1> { Math.random() > 0.9 ? <hr/> : <img alt='some image'/> } </h1> */}
+        {/* Если не нужно возвращать картинку, то пишем : underfined или : null (?? жаждет найти u или n, но их НЕ выбирает) */}
+        {/* Н-р: 2 ?? null(увидим 2), null ?? 2(увидим 2), underfined ?? null(увидим null) */}
+        {/* в {} надо писать такое вычисление, кот. что-нибудь возвращает */}
+      <h1> { TEXT } </h1> 
+      {/* здесь внутри интерпретируем это как js, стопаем jsx */}
+      <p>учусь на первом занятии</p>
+      <hr/>
+    </div>
+    <div>
+      <label htmlFor='inp'>click me</label>
+      <input id='inp' type='text'/>
+    </div>
+  </React.Fragment>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
